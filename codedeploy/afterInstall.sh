b9ba69e1d368bf0ba2f315216bb3911353d6b121
Pavan Rao <pavan47kalyan@gmail.com>
2020-06-28 11:11:56 -0400

    Modified app spec hooks

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -2,4 +2,3 @@ cd /home/ubuntu
 npm install
 cd frontend/
 npm install
-REACT_APP_IP_ADDRESS=18.212.68.91 npm start
--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,2 +1,4 @@
-cd //
+pwd
 ls -al
+cd /home/ubuntu/frontend
+REACT_APP_IP_ADDRESS=18.212.68.91 npm start
