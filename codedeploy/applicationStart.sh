6cb58c49d9ea9e6f7a421e805aa269d3dd67df7d
Pavan Rao <pavan47kalyan@gmail.com>
2020-06-28 05:20:35 -0400

    Modified codedeploy scripts

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,11 +1,2 @@
-cd /etc/profile.d
-
-env
-
-FILE=userdata.sh
-while ! test -f "$FILE"; do
-    sleep 30s
-    echo "$FILE not exist"
-done
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
+cd //
+ls -al
