cdd343ca5459fe5f7d1c0a610d2b5a755cccf1b4
Pavan Rao <pavan47kalyan@gmail.com>
2020-06-29 13:21:47 -0400

    Modified config yml

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,3 +1,14 @@
+cd /
+sudo chmod 757 data.txt
+sudo cp data.txt /home/ubuntu
+cd home
+cd ubuntu
+sudo chmod 757 ip.txt
+sudo chmod 757 data.txt
+cat data.txt
+cat ip.txt
+cat data.txt ip.txt > .env
+cat .env
 cd /home/ubuntu
 pwd
 ls -al
