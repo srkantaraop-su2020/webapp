b8884c7754a0855681d365473ebdea033768e265
Pavan Rao <pavan47kalyan@gmail.com>
2020-06-29 17:09:34 -0400

    Modified code deploy scripts 2

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -7,14 +7,12 @@ sudo source .env
 set +a
 echo "Set IP ADDRESS as env"
 echo $IP_ADDRESS
-set | grep IP_ADDRESS
 echo "Finished verifying set env"
 pwd
 echo "Installing pm2 for the server........."
 sudo npm install pm2 -g -f
 echo "Finished Installing pm2 for the server"
 echo "Starting node server now......."
-IP_ADDRESS=$IP_ADDRESS pm2 start server.js
 echo "Node server is up and running, find the active servers below"
 pm2 list
 cd frontend/
