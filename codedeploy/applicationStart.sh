12d8fc34490ae904f1a2bab94ba986d6c2bfb558
Pavan Rao <pavan47kalyan@gmail.com>
2020-06-29 13:50:39 -0400

    Modified application start

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,8 +1,8 @@
 cd /home/ubuntu
 echo "Displaying contents of ip2.txt..........."
-cat ip2.txt
+cat ip2.sh
 echo "Finished displaying contents of ip2.txt"
-set -a source ip2.txt
+set -a source ip2.sh
 set +a
 echo "Set IP ADDRESS as env"
 echo $IP_ADDRESS
