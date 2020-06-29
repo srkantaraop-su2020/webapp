b8b4beeaba09172e38834f5b2a4575b756d11885
Pavan Rao <pavan47kalyan@gmail.com>
2020-06-29 14:47:22 -0400

    Modified code deploy scripts

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -1,4 +1,7 @@
 echo "Inside application stop"
+cd /home/ubuntu
+pwd
+echo "Currently in home/ubuntu"
 ls -al
 sudo rm -rf app frontend test *.sh *.html *.yml *.json *.js *.txt .env
 ls -al
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,4 +1,7 @@
-echo "Inside application stop"
+echo "Inside before install"
+cd /home/ubuntu
+pwd
+echo "Currently in home/ubuntu"
 ls -al
 sudo rm -rf app frontend test *.sh *.html *.yml *.json *.js *.txt .env
 ls -al
