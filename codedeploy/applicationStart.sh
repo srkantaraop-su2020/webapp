60b6ccbd40e073b0954897c2d2f6acd916fc03fd
Pavan Rao <pavan47kalyan@gmail.com>
2020-07-09 15:37:59 -0400

    Chanhed restart logic

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -26,14 +26,11 @@ pm2 list
 echo "Starting cloud watch agent now..."
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
 echo "Setting up pm2 restart service now"
-pm2 startup
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-sudo mkdir -p /etc/systemd/system/pm2-ubuntu.service.d
-sudo touch /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
-sudo echo "[Service]" >> /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
-sudo echo "Restart=always" >> /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
-sudo echo "RestartSec=3" >> /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
+cd /home/ubuntu/
 sudo systemctl daemon-reload
-pm2 save 
-sudo systemctl status pm2-ubuntu
+systemctl restart application.service
+sudo mv application.service /lib/systemd/system
+chmod +x server.js
+sudo systemctl enable application.service
+sudo systemctl start application.service
 echo "Finised setting up pm2 restart service"
