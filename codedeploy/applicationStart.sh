693209f17b544cba5d96b16927e96583e8b4933c
Pavan Rao <pavan47kalyan@gmail.com>
2020-07-09 15:03:49 -0400

    Rearranged pm2 restart logic

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -4,18 +4,6 @@ echo "$value"
 echo "Installing pm2 for the server........."
 sudo npm install pm2 -g -f
 echo "Finished Installing pm2 for the server"
-echo "Setting up pm2 restart service now"
-pm2 startup
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-sudo mkdir -p /etc/systemd/system/pm2-ubuntu.service.d
-sudo touch /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
-sudo echo "[Service]" >> /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
-sudo echo "Restart=always" >> /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
-sudo echo "RestartSec=3" >> /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
-sudo systemctl daemon-reload
-pm2 save 
-sudo systemctl status pm2-ubuntu
-echo "Finised setting up pm2 restart service"
 echo "Starting node server now......."
 sudo sed -i -e "s|ipAddress|$value|g" server.js
 IP_ADDRESS=$IP_ADDRESS pm2 start server.js
@@ -37,3 +25,15 @@ echo "Node server is up and running, find the active servers below"
 pm2 list
 echo "Starting cloud watch agent now..."
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
+echo "Setting up pm2 restart service now"
+pm2 startup
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+sudo mkdir -p /etc/systemd/system/pm2-ubuntu.service.d
+sudo touch /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
+sudo echo "[Service]" >> /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
+sudo echo "Restart=always" >> /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
+sudo echo "RestartSec=3" >> /etc/systemd/system/pm2-ubuntu.service.d/10_auto_restart_pm2.conf
+sudo systemctl daemon-reload
+pm2 save 
+sudo systemctl status pm2-ubuntu
+echo "Finised setting up pm2 restart service"
